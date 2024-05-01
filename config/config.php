b5f3fd83b0a2b4e1eb49fa3bb1fd0e0117915689
raanki <rabalone94@gmail.com>
2024-05-01 13:28:02 +0200

    index

--- config/config.php
+++ config/config.php
@@ -2,7 +2,19 @@
 
 const CONFIG_PATH = '/config/config.php';
 const INCLUDE_PATH = __DIR__ . '/resources/include/';
-const BOOTSTRAP_PATH = __DIR__ . '/../resources/include/bootstrap.html';
-const JQUERY_PATH = __DIR__ . '/../resources/include/jquery.html';
+const CSS_PATH = __DIR__ . '/../' . 'resources/css/';
+const BOOTSTRAP_PATH = __DIR__ . '/../' . 'resources/include/bootstrap.html';
+const JQUERY_PATH = __DIR__ . '/../' . 'resources/include/jquery.html';
+
+#COLOR
+
+const VIOLET_DARK = "#271B2B";
+const BLUE_DARK = "#24344E";
+const BLUE_CHILL = "#327281";
+const VIOLET_CHILL = "#523148";
+const RED_CHILL = "#AF4743";
+const ROSE_CHILL = "#AF4743";
+
+
 
 
--- view/index.php
+++ view/index.php
@@ -1,3 +1,4 @@
+<?php require_once '../config/config.php' ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,16 +7,60 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Menteur Menteur</title>
-    <?php require_once '../config/config.php' ?>
+
+
+    <link rel="icon" href="resources/image/logo.png" type="image/png">
+
 
     <?php require_once BOOTSTRAP_PATH ?>
-    <?php require_once  JQUERY_PATH ?>
+    <?php require_once JQUERY_PATH ?>
 </head>
 <body>
 
-    <div class="container">
+<div class="container">
 
+<div class="row mx-auto justify-content-center d-flex row-fluid text-center mt-5">
+    <h3 class="text-light">
+        Bienvenue sur Menteur Menteur !
+        <small class="text-light-emphasis">By ranki</small>
+    </h3>
+</div>
+
+    <img src="resources/image/logo.png" class="img-fluid ms-auto me-auto justify-content-center d-flex " alt="logo">
+
+    <div class="row menu d-grid gap-2 mx-auto col-6 align-items-center mt-5">
+
+        <div class="row">
+            <button class="title btn btn-primary">Créer une partie</button>
+        </div>
+
+        <div class="row mt-3">
+
+            <div class="input-group d-flex justify-content-center">
+
+                <div class="code-field d-flex me-2">
+                    <span class="input-group-text style-input text-bg-dark border border-0 rounded-end-0 w-100"
+                          id="basic-addon3">CODE</span>
+
+                    <input type="text" class="form-control style-input text-bg-dark border border-0 rounded-start-0"
+                           id="code-field" aria-describedby="basic-addon3 basic-addon4" placeholder="Ex : AB95 ...">
+
+                </div>
+
+                <div class="join-btn col-4">
+                    <button class="title btn btn-primary">Rejoindre une partie</button>
+                </div>
+
+            </div>
+
+        </div>
     </div>
+</div>
+
 
 </body>
 </html>
+
+<style>
+    <?php require_once  CSS_PATH . 'index.css' ?>
+</style>
