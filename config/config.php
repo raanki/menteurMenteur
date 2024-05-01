164aa63131c9204389f53301ae4d22121a6c2ae0
raanki <rabalone94@gmail.com>
2024-05-01 12:16:20 +0200

    first commit

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+const CONFIG_PATH = '/config/config.php';
+const INCLUDE_PATH = __DIR__ . '/resources/include/';
+const BOOTSTRAP_PATH = __DIR__ . '/../resources/include/bootstrap.html';
+const JQUERY_PATH = __DIR__ . '/../resources/include/jquery.html';
+
+
---
+++ view/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menteur Menteur</title>
+    <?php require_once '../config/config.php' ?>
+
+    <?php require_once BOOTSTRAP_PATH ?>
+    <?php require_once  JQUERY_PATH ?>
+</head>
+<body>
+
+    <div class="container">
+
+    </div>
+
+</body>
+</html>
